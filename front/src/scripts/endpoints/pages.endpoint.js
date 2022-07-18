e76647837442d85f518a288206a07abee39ff17d
Xaviju <xaviju@gmail.com>
2022-07-18 16:06:15 +0200

    API chulo :alien:

---
+++ front/src/scripts/endpoints/pages.endpoint.js
@@ -0,0 +1,44 @@
+import { config } from "../../assets/config";
+
+// # Page data model
+
+// id (UUID): autogenerado. Este ID identifica la página independientemente de su orden
+// order (Integer): cuanto menor sea irá por delante en el cuaderno. Idealmente deberían cubrirse todos los números pero tampoco pasa nada si no porque se accede por ID
+// type (Enumerado): representa el tipo de página
+// slots (Array de Slot)
+
+const createPage = (scrapbookId) => {
+  const url = `${config.apiUrl}/scrapbooks/${scrapbookId}/pages`;
+
+  fetch(url, {
+    method: "PUT",
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+};
+
+const modifyPage = (scrapbookId, pageId, data) => {
+  const url = `${config.apiUrl}/scrapbooks/${scrapbookId}/pages/${pageId}`;
+
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+};
+
+const deletePage = (scrapbookId, pageId) => {
+  const url = `${config.apiUrl}/scrapbooks/${scrapbookId}/pages/${pageId}`;
+
+  fetch(url, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+};
+
+export { createPage, modifyPage, deletePage };
---
+++ front/src/scripts/endpoints/scrapbooks.endpoint.js
@@ -0,0 +1,73 @@
+import { config } from "../../assets/config";
+
+// # Scrapbook data model
+
+// id (UUID): Autogenerado
+// name (string): Nombre del cuaderno
+// dateStart (string formato YYYY-MM-DD)
+// dateFinish (string) formato YYYY-MM-DD)
+// destination (string)
+// people (Array de String): representa los que viajan, en principio el niño estará en 0, madre en 1 y padre en 2
+// ej: [“Paula”, “Yara”, “Javier”]
+// pages (Array de Pages): el array estará ordenado por el campo “order” de la página
+
+const listScrapbooks = () => {
+  const url = `${config.apiUrl}/scrapbooks`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+};
+
+const createScrapbook = (data) => {
+  const url = `${config.apiUrl}/scrapbooks`;
+
+  fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+};
+
+const getScrapbook = (id) => {
+  const url = `${config.apiUrl}/scrapbooks/${id}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+};
+
+const modifyScrapbook = (id, data) => {
+  const url = `${config.apiUrl}/scrapbooks/${id}`;
+
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+};
+
+const deleteScrapbook = (id) => {
+  const url = `${config.apiUrl}/scrapbooks/${id}`;
+
+  fetch(url, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+};
+
+export {
+  listScrapbooks,
+  createScrapbook,
+  getScrapbook,
+  modifyScrapbook,
+  deleteScrapbook,
+};
