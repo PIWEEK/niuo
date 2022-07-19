215e3abb7fed6fcd75fc092d4533d3a2c2cc9e86
Xaviju <xaviju@gmail.com>
2022-07-19 11:51:52 +0200

    Mocks :children_crossing:

--- front/src/scripts/app.js
+++ front/src/scripts/app.js
@@ -1,3 +1,17 @@
-import { multiply } from "./endpoints/scrapbooks.endpoint";
+import { getScrapbook } from "./endpoints/scrapbooks.endpoint";
 
-console.log(multiply(2));
+const generatePages = () => {
+  const pages = getScrapbook();
+
+  const pagesList = document.querySelector('[data-query="pages-list"]');
+
+  pages.forEach((page) => {
+    console.log({ page });
+    const pageEl = document.createElement("div");
+    pageEl.classList.add("card");
+    pageEl.setAttribute("data-type", page.type);
+    pagesList.append(pageEl);
+  });
+};
+
+generatePages();
