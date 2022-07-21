99da58a7291d81d6f30690f0726474bba6405f7a
andy <andres.gonzalez79@gmail.com>
2022-07-21 14:27:01 +0200

    more maketa bakaleta

--- front/src/scripts/types/textImage.js
+++ front/src/scripts/types/textImage.js
@@ -3,8 +3,11 @@ const buildTextImagePage = (pageEl, page, index, scrapbook) => {
   pageEl.innerHTML = `
   <div class="summary">El viaje de ${scrapbook.who} a ${scrapbook.where}</div>
   <p class="font-ligature intro">
-  ¡Qué ilusión! TEXTO CON IMAGEN
+  ¡Qué ilusión! Nos vamos a
+    <span class="where">${scrapbook.where}<span>
   </p>
+  <div class="image-decoration"></div>
+  <img src="" class="image" />
     `;
 };
 
